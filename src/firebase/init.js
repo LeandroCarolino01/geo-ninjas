6c42ab25558ede902196c4a177d341d266819b4c
leandro <leandro@isset.nl>
2018-12-19 11:17:58 +0100

    init

---
+++ src/firebase/init.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyAFB2VK33pWO483PUF6fQvg23WyJANW3xM",
+    authDomain: "geo-ninjas-b9053.firebaseapp.com",
+    databaseURL: "https://geo-ninjas-b9053.firebaseio.com",
+    projectId: "geo-ninjas-b9053",
+    storageBucket: "geo-ninjas-b9053.appspot.com",
+    messagingSenderId: "208465908090"
+  };
+  firebase.initializeApp(config);
